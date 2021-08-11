6cd3667070b683c6f0b78b381996f3872bc64f2a
chiyuuarchive <85938105+chiyuuarchive@users.noreply.github.com>
2021-08-11 14:19:30 +0200

    Setting up the files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import {
+  BrowserRouter,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+// Import API key
+import APIKey from "./config";
+
+export default class App extends Component {
+
+  render () {
+    return (
+      <h1>Hello!</h1>
+    );
+  }
+}
